917be8cf74cf1962f22f327ea0eb7d928b24d1c8
SIMRAN SINGH <SIMRAN514@GMAIL.COM>
2018-11-01 23:02:45 -0500

    Add project files.

---
+++ Singular Integer Right Triangles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singular_Integer_Right_Triangles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
